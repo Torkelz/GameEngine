e08d2e4a0dc09482e6916c2531c3df2f49d61096
Smurfa <smurfa-021@hotmail.com>
2014-09-04 11:37:04 +0200

    Chunk separation in progress.

--- GameEngine/ToolKit/PoolAllocator.cpp
+++ GameEngine/ToolKit/PoolAllocator.cpp
@@ -1,24 +1,61 @@
 #include "PoolAllocator.h"
+#include <malloc.h>
+#include <string>
 
-
-Allocator::PoolAllocator::PoolAllocator(UINT p_ItemSize, UINT p_NumItem)
+namespace Allocator
 {
-}
+	PoolAllocator::PoolAllocator(UINT p_ItemSize, UINT p_NumItem)
+	{
+		UINT totalSize = p_ItemSize * p_NumItem;
+		m_Data = (char*)malloc(totalSize);
+		if (!m_Data)
+		{
+			std::string errorMsg = "Bad allocation at line: " + std::to_string(__LINE__) + ", in file: " + __FILE__;
+			throw errorMsg;
+		}
+		m_FirstFree = m_Data;
+		m_ItemSize = p_ItemSize;
 
+		for (UINT i = 0; i < totalSize; i += p_ItemSize)
+		{
+			m_Data[i] = +p_ItemSize;
+		}
+	}
 
-Allocator::PoolAllocator::~PoolAllocator()
-{
-}
+	PoolAllocator::PoolAllocator(char *p_Buffer, UINT p_ItemSize, UINT p_NumItem)
+	{
+		m_Data = p_Buffer;
+		
+		//TODO: Move pointer in buffer forwards and such
+		m_FirstFree = m_Data;
+		m_ItemSize = p_ItemSize;
+	}
 
-void *Allocator::PoolAllocator::allocate(void)
-{
-	return nullptr;
-}
+	PoolAllocator::~PoolAllocator()
+	{
+	}
 
-void Allocator::PoolAllocator::deallocate(void *p_Position)
-{
-}
+	void *PoolAllocator::allocate(void)
+	{
+		//This is stack based allocation! do not use here!
+		//void *allocation = m_Data;
+		//m_Data += m_ItemSize;
 
-void Allocator::PoolAllocator::clear()
-{
+		//return allocation;
+
+		//TODO: Pick out next item in free list and return it?
+
+		//return next item in free list
+		return nullptr;
+	}
+
+	void PoolAllocator::deallocate(void *p_Position)
+	{
+
+	}
+
+	void PoolAllocator::clear()
+	{
+
+	}
 }
--- GameEngine/ToolKit/PoolAllocator.h
+++ GameEngine/ToolKit/PoolAllocator.h
@@ -9,9 +9,11 @@ namespace Allocator
 	private:
 		char *m_Data;
 		void *m_FirstFree;
+		UINT m_ItemSize;
 
 	public:
 		PoolAllocator(UINT p_ItemSize, UINT p_NumItem);
+		PoolAllocator(char *p_Buffer, UINT p_ItemSize, UINT p_NumItem);
 		~PoolAllocator();
 		void *allocate(void);
 		void deallocate(void *p_Position);
