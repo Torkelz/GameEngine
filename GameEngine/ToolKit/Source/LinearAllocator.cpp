d7855e04718eb97e360c04a09ca6bc0fb48a6eb1
Torkelz <and_re1@hotmail.com>
2014-09-04 11:01:58 +0200

    Added definitions and begun implementations of LinearAllocator class.

---
+++ GameEngine/ToolKit/Source/LinearAllocator.cpp
@@ -0,0 +1,19 @@
+#include "LinearAllocator.h"
+
+namespace Allocator
+{
+	LinearAllocator::LinearAllocator(UINT p_Size)
+	{
+	}
+
+
+	LinearAllocator::~LinearAllocator()
+	{
+	}
+
+	void *LinearAllocator::alloc(UINT p_Size)
+	{
+
+	}
+
+}
--- GameEngine/ToolKit/Source/LinearAllocator.h
+++ GameEngine/ToolKit/Source/LinearAllocator.h
@@ -5,8 +5,24 @@ namespace Allocator
 	class LinearAllocator
 	{
 	public:
-		LinearAllocator();
+		typedef unsigned int UINT;
+
+	private:
+		UINT m_Marker;
+		UINT m_Size;
+		char *m_Buffer;
+	public:
+	public:
+		LinearAllocator(UINT p_Size);
 		~LinearAllocator();
+
+		void *alloc(UINT p_Size);
+		void freeToMarker(const UINT &p_Marker);
+		void clear();
+
+		const UINT &getMarker() const;
+
+	private:
 	};
 }
 
