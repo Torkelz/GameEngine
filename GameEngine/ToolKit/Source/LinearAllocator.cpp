7935760031d0bc30e29fa4080a985de0200e04ac
Torkelz <and_re1@hotmail.com>
2014-09-04 11:06:20 +0200

    Created empty implementations for the LinearAllocator class.

--- GameEngine/ToolKit/Source/LinearAllocator.cpp
+++ GameEngine/ToolKit/Source/LinearAllocator.cpp
@@ -11,9 +11,23 @@ namespace Allocator
 	{
 	}
 
-	void *LinearAllocator::alloc(UINT p_Size)
+	void* LinearAllocator::alloc(UINT p_Size)
 	{
 
 	}
 
+	void LinearAllocator::freeToMarker(const UINT &p_Marker)
+	{
+
+	}
+
+	void LinearAllocator::clear()
+	{
+
+	}
+
+	const LinearAllocator::UINT& LinearAllocator::getMarker() const
+	{
+
+	}
 }
