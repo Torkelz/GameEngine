c11ebe06e559e6351e39fad8efe7667cf5746bd5
Torkelz <and_re1@hotmail.com>
2014-09-05 20:46:04 +0200

    Created two test cases in Main.cpp to try out the timer.

--- GameEngine/GameEngine/Source/main.cpp
+++ GameEngine/GameEngine/Source/main.cpp
@@ -1,9 +1,37 @@
 #include "IToolKit.h"
 
+#include "HighPerformanceTimer.h"
+
 #include <iostream>
+#include <functional>
+#include <string>
+
+void functionToBeMeasured()
+{
+	Allocator::LinearAllocator alloc(1100);
+	for (unsigned int i = 0; i < 250; ++i)
+	{
+		int *k = new(alloc) int;
+		*k = i;
+	}
+}
+void functionToBeMeasured2()
+{
+	for (unsigned int i = 0; i < 250; ++i)
+	{
+		int *k = new int;
+		*k = i;
+	}
+}
 
 int main(int /*argc*/, char* /*argv*/[])
 {
+	HighPerformanceTimer timer;
+
+	double f1 = timer.measureFunction(std::bind(functionToBeMeasured), 100);
+	double f2 = timer.measureFunction(std::bind(functionToBeMeasured2), 100);
+
+	std::cout << std::to_string(f1) << " ? " << std::to_string(f2) << std::endl;
 	std::system("PAUSE");
 	return 0;
 }
