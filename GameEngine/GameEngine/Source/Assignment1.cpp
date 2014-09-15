87d12aec2c43206cc126796e82a90050777eb7a6
Tokelz <and_re1@hotmail.com>
2014-09-15 11:13:16 +0200

    Added assignment class.

---
+++ GameEngine/GameEngine/Source/Assignment1.cpp
@@ -0,0 +1,11 @@
+#include "Assignment1.h"
+
+
+Assignment1::Assignment1(void)
+{
+}
+
+
+Assignment1::~Assignment1(void)
+{
+}
---
+++ GameEngine/GameEngine/Source/Assignment1.h
@@ -0,0 +1,8 @@
+#pragma once
+class Assignment1
+{
+public:
+	Assignment1(void);
+	~Assignment1(void);
+};
+
