5af27c571f4d389e94cbba3d3474e43481170846
unknown <BTH@H473-0001.students.ad.bth.se>
2014-10-22 10:16:50 +0200

    Open file dialog added.

--- GameEngine/AssetPackager/Form1.Designer.cs
+++ GameEngine/AssetPackager/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loadFiles = new System.Windows.Forms.Button();
+            this.createFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loadFiles
+            // 
+            this.loadFiles.Location = new System.Drawing.Point(345, 281);
+            this.loadFiles.Name = "loadFiles";
+            this.loadFiles.Size = new System.Drawing.Size(75, 23);
+            this.loadFiles.TabIndex = 0;
+            this.loadFiles.Text = "Load files...";
+            this.loadFiles.UseVisualStyleBackColor = true;
+            this.loadFiles.Click += new System.EventHandler(this.loadFiles_Click);
+            // 
+            // createFiles
+            // 
+            this.createFiles.Location = new System.Drawing.Point(514, 280);
+            this.createFiles.Name = "createFiles";
+            this.createFiles.Size = new System.Drawing.Size(97, 23);
+            this.createFiles.TabIndex = 1;
+            this.createFiles.Text = "Create package";
+            this.createFiles.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 512);
+            this.Controls.Add(this.createFiles);
+            this.Controls.Add(this.loadFiles);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadFiles;
+        private System.Windows.Forms.Button createFiles;
     }
 }
 
--- GameEngine/AssetPackager/Form1.cs
+++ GameEngine/AssetPackager/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,38 @@ namespace AssetPackager
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loadFiles_Click(object sender, EventArgs e)
+        {
+            if(sender == null)
+                return;
+
+            Stream myStream;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
         }
     }
 }
