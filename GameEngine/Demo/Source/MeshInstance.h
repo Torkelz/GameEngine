0175ab43018ba8180a90412349cd3ff490de3c8b
torkelz <and_re1@hotmail.com>
2014-10-24 10:50:49 +0200

    MeshInstance cleanup and comments.

--- GameEngine/Demo/Source/MeshInstance.h
+++ GameEngine/Demo/Source/MeshInstance.h
@@ -12,6 +12,10 @@ struct MeshInstance
 	mutable bool m_IsCalculated;
 	mutable DirectX::XMFLOAT4X4 world;
 
+	/**
+	* Gets a mesh instance's world matrix.
+	* @return, returns a XMFLOAT4X4 matrix.
+	*/
 	const DirectX::XMFLOAT4X4& getWorldMatrix() const
 	{
 		if (!m_IsCalculated)
@@ -21,18 +25,30 @@ struct MeshInstance
 		return world;
 	}	
 
+	/**
+	* Sets a mesh instance's position.
+	* @param p_Position, Vector3 in world coordinates.
+	*/
 	void MeshInstance::setPosition(const Vector3& p_Position)
 	{
 		position = p_Position;
 		m_IsCalculated = false;
 	}
 
+	/**
+	* Sets a mesh instance's rotation.
+	* @param p_Rotation, Vector3(pitch,yaw,roll) in degrees.
+	*/
 	void MeshInstance::setRotation(const Vector3& p_Rotation)
 	{
 		rotation = p_Rotation;
 		m_IsCalculated = false;
 	}
 
+	/**
+	* Sets a mesh instance's scale.
+	* @param p_Scale, Vector(ScaleX,ScaleY,ScaleZ).
+	*/
 	void MeshInstance::setScale(const Vector3& p_Scale)
 	{
 		scale = p_Scale;
