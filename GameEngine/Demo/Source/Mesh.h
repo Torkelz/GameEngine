39c188db6a6334737141bc433a47dfe978336055
torkelz <and_re1@hotmail.com>
2014-10-24 10:41:46 +0200

    Mesh cleanup and comments.

--- GameEngine/Demo/Source/Mesh.h
+++ GameEngine/Demo/Source/Mesh.h
@@ -1,9 +1,10 @@
 #pragma once
-#include <memory>
 #include "Buffer.h"
 #include "Shader.h"
 #include "Utilities.h"
 #include "IResourceManager.h"
+
+#include <memory>
 
 struct Mesh
 {
@@ -17,29 +18,9 @@ struct Mesh
 	Mesh() :
 		shader(nullptr),
 		buffer(nullptr)
-
-	{
-
-	}
-	Mesh(Mesh&& p_Other) :
-		buffer(std::move(p_Other.buffer)),
-		shader(p_Other.shader),
-		indexBuffer(std::move(p_Other.indexBuffer)),
-		diffusemaps(std::move(p_Other.diffusemaps)),
-		faceGroups(std::move(p_Other.faceGroups))
 	{
 	}
 
-	Mesh& operator=(Mesh&& p_Other)
-	{
-		std::swap(buffer, p_Other.buffer);
-		std::swap(shader, p_Other.shader);
-		std::swap(indexBuffer, p_Other.indexBuffer);
-		std::swap(diffusemaps, p_Other.diffusemaps);
-		std::swap(faceGroups, p_Other.faceGroups);
-
-		return *this;
-	}
 	~Mesh()
 	{
 		shader = nullptr;
@@ -50,6 +31,40 @@ struct Mesh
 			SAFE_RELEASE(s);
 		}
 	}
+
+	/**
+	* Copy constructor.
+	*/
+	Mesh(Mesh&& p_Other) :
+		buffer(std::move(p_Other.buffer)),
+		shader(p_Other.shader),
+		indexBuffer(std::move(p_Other.indexBuffer)),
+		diffusemaps(std::move(p_Other.diffusemaps)),
+		faceGroups(std::move(p_Other.faceGroups))
+	{
+	}
+
+	/**
+	* Assignment constructor.
+	*/
+	Mesh& operator=(Mesh&& p_Other)
+	{
+		std::swap(buffer, p_Other.buffer);
+		std::swap(shader, p_Other.shader);
+		std::swap(indexBuffer, p_Other.indexBuffer);
+		std::swap(diffusemaps, p_Other.diffusemaps);
+		std::swap(faceGroups, p_Other.faceGroups);
+
+		return *this;
+	}
+
+	
+
+	/**
+	* Replaces a already existing shaderresourceview with a new one.
+	* @param p_newSRV, the new shaderresourceview. Is not allowed to be nullptr.
+	* @param p_DiffuseIndex, the index of the shaderresourceview to be replaced.
+	*/
 	void setSRV(ID3D11ShaderResourceView* p_newSRV, int p_DiffuseIndex)
 	{
 		std::swap(diffusemaps.at(p_DiffuseIndex), p_newSRV);
