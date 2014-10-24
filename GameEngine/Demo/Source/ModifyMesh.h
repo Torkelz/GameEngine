1036a4d5eb224a961d53091293072ec551d8761d
torkelz <and_re1@hotmail.com>
2014-10-24 10:55:41 +0200

    ModifyMesh cleanup and comments.

--- GameEngine/Demo/Source/ModifyMesh.h
+++ GameEngine/Demo/Source/ModifyMesh.h
@@ -8,10 +8,36 @@ private:
 	Render *m_Render;
 	static std::unique_ptr<ModifyMesh> m_Instance;
 public:
+	/**
+	* Initialized the ModifyMesh singleton.
+	* @param p_Render, valid pointer to Render.
+	*/
 	static void initialize(Render *p_Render);
 
+	/**
+	* Sets a mesh instance's position.
+	* @param p_InstanceId, the mesh instance's graphical identifier.
+	* @param p_Position, Vector3 in world coordinates.
+	*/
 	static void setMeshPosition(Render::UINT p_InstanceId, Vector3 p_Position);
+
+	/**
+	* Sets a mesh instance's rotation.
+	* @param p_InstanceId, the mesh instance's graphical identifier.
+	* @param p_Rotation, Vector3(pitch,yaw,roll) in degrees.
+	*/
 	static void setMeshRotation(Render::UINT p_InstanceId, Vector3 p_YawPitchRoll);
+
+	/**
+	* Sets a mesh instance's scale.
+	* @param p_InstanceId, the mesh instance's graphical identifier.
+	* @param p_Scale, Vector(ScaleX,ScaleY,ScaleZ).
+	*/
 	static void setMeshScale(Render::UINT p_InstanceId, Vector3 p_Scale);
-	static ModifyMesh *getInstance();
+
+	/**
+	* Gets the singleton instance of ModifyMesh.
+	* @return, returns a ModifyMesh pointer.
+	*/
+	static ModifyMesh *getInstance(void);
 };
